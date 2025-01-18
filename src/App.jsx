adf1e2a110f0cb4acbcea0e2a3d25a0e22651ca8
Luka Talakvadze <lukatalakvadze@gmail.com>
2025-01-18 11:04:36 +0400

    feat: add header component and two more page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { FaLink } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { IoEyeOutline } from "react-icons/io5";
+import { Link, useLocation } from "react-router-dom";
+
+function Header() {
+  const location = useLocation();
+  return (
+    <div className="w-full bg-[#ffffff] py-4 pl-6 pr-4 flex items-center justify-between">
+      <img src="/icons/logo-icon.svg" alt="logo-icon" />
+      <div className="flex items-center">
+        <Link to={"/home"}>
+          <div
+            style={
+              location.pathname === "/home"
+                ? { backgroundColor: "#efebff" }
+                : { backgroundColor: "transparent" }
+            }
+            className="px-[27px] py-[11px] rounded-lg"
+          >
+            <FaLink
+              color={location.pathname === "/home" ? "#633cff" : "#737373"}
+            />
+          </div>
+        </Link>
+        <Link to={"/profile"}>
+          <div
+            style={
+              location.pathname === "/profile"
+                ? { backgroundColor: "#efebff" }
+                : { backgroundColor: "transparent" }
+            }
+            className="px-[27px] py-[11px] rounded-lg"
+          >
+            <CgProfile
+              color={location.pathname === "/profile" ? "#633cff" : "#737373"}
+            />
+          </div>
+        </Link>
+      </div>
+      <div
+        style={{ border: "solid 1px #633cff" }}
+        className="py-[11px] px-4 rounded-lg"
+      >
+        <IoEyeOutline color="#633cff" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
